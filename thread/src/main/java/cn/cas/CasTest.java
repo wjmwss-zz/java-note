6dc38052fdae5482862d28bda0290fc0d71a1adf
wjm <wjmcoo@outlook.com>
2020-11-22 18:35:08 +0800

    update

---
+++ thread/src/main/java/cn/cas/CasTest.java
@@ -0,0 +1,18 @@
+package cn.cas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @Author: wjm
+ * @date: 2020/3/27 15:58
+ */
+@SpringBootApplication
+public class CasTest {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApp.class, args);
+    }
+}
